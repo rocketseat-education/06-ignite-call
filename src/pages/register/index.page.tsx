ebe3085a2f54dd1cfa6a69490e2a4c7cee445947
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-12-14 10:58:55 -0300

    Cadastro de usuário - Estrutura visual do cadastro

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,37 @@
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+
+import { Container, Form, Header } from './styles'
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+
+      <Form as="form">
+        <label>
+          <Text size="sm">Nome de usuário</Text>
+          <TextInput prefix="ignite.com/" placeholder="seu-usuário" />
+        </label>
+
+        <label>
+          <Text size="sm">Nome completo</Text>
+          <TextInput placeholder="Seu nome" />
+        </label>
+
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
---
+++ src/pages/register/styles.ts
@@ -0,0 +1,33 @@
+import { styled, Box, Heading, Text } from '@ignite-ui/react'
+
+export const Container = styled('main', {
+  maxWidth: 572,
+  margin: '$20 auto $4',
+  padding: '0 $4',
+})
+
+export const Header = styled('div', {
+  padding: '0 $6',
+
+  [`> ${Heading}`]: {
+    lineHeight: '$base',
+  },
+
+  [`> ${Text}`]: {
+    color: '$gray200',
+    marginBottom: '$6',
+  },
+})
+
+export const Form = styled(Box, {
+  marginTop: '$6',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$4',
+
+  label: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '$2',
+  },
+})

02980f4c245c8d61268a7f99284ef515b5932afe
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-12-15 13:56:37 -0300

    Conexão com calendário - Conteúdo da sessão do Next Auth

--- src/pages/api/auth/[...nextAuth].api.ts
+++ src/pages/api/auth/[...nextAuth].api.ts
@@ -40,6 +40,12 @@ export function buildNextAuthOptions(
 
         return true
       },
+      async session({ session, user }) {
+        return {
+          ...session,
+          user,
+        }
+      },
     },
   }
 }
--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -17,6 +17,8 @@ export default function Register() {
     await signIn('google')
   }
 
+  console.log(session)
+
   return (
     <Container>
       <Header>

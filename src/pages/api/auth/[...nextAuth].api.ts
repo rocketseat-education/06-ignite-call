618366e8022c936682584fc2302fd8ff41702d16
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-12-15 11:43:29 -0300

    Conexão com calendário - Permissão de acesso ao calendário

--- src/pages/api/auth/[...nextAuth].api.ts
+++ src/pages/api/auth/[...nextAuth].api.ts
@@ -6,7 +6,24 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+        },
+      },
     }),
   ],
+  callbacks: {
+    async signIn({ account }) {
+      if (
+        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+      ) {
+        return '/register/connect-calendar?error=permissions'
+      }
+
+      return true
+    },
+  },
 }
 export default NextAuth(authOptions)

9709394b7a5c032837112e2fddbd8dae9fb14085
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-12-15 10:43:15 -0300

    Cadastro de usuário - Manipulando cookies no Next.js

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 import { prisma } from '../../../lib/prisma'
 
 export default async function handler(
@@ -11,11 +12,28 @@ export default async function handler(
 
   const { name, username } = req.body
 
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'Username already taken.',
+    })
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   })
 
   return res.status(201).json(user)
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { AxiosError } from 'axios'
 import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
@@ -49,7 +50,12 @@ export default function Register() {
         username: data.username,
       })
     } catch (err) {
-      console.log(err)
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+
+      console.error(err)
     }
   }
 

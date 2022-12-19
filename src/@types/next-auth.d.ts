09836a3f69758ce6e020a9187d0bb50789412acb
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-12-19 16:26:22 -0300

    Configurando disponibilidade - Persistindo intervalos

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -8,4 +8,8 @@ declare module 'next-auth' {
     username: string
     avatar_url: string
   }
+
+  interface Session {
+    user: User
+  }
 }
--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,6 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { unstable_getServerSession } from 'next-auth'
+import { z } from 'zod'
+import { prisma } from '../../../lib/prisma'
 import { buildNextAuthOptions } from '../auth/[...nextAuth].api'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,5 +28,24 @@ export default async function handler(
     buildNextAuthOptions(req, res),
   )
 
-  return res.json({ session })
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
 }

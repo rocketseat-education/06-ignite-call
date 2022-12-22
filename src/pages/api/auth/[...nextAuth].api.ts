7aca1b67ade3d07cc0817383ebcc20b5e3d407e4
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-12-22 14:16:55 -0300

    Agendamento - Refresh de token do Google

--- src/pages/api/auth/[...nextAuth].api.ts
+++ src/pages/api/auth/[...nextAuth].api.ts
@@ -15,6 +15,9 @@ export function buildNextAuthOptions(
         clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
         authorization: {
           params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
             scope:
               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
           },

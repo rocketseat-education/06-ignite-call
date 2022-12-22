e54caa85d82b6eaaf0afdca1e7854f354307f12a
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-12-22 14:55:11 -0300

    Deploy do projeto - Deploy no Planetscale

---
+++ prisma/migrations/20221222115134_add_database_indexes/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE `accounts` DROP FOREIGN KEY `accounts_user_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `schedulings` DROP FOREIGN KEY `schedulings_user_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `sessions` DROP FOREIGN KEY `sessions_user_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `user_time_intervals` DROP FOREIGN KEY `user_time_intervals_user_id_fkey`;
+
+-- RenameIndex
+ALTER TABLE `accounts` RENAME INDEX `accounts_user_id_fkey` TO `accounts_user_id_idx`;
+
+-- RenameIndex
+ALTER TABLE `schedulings` RENAME INDEX `schedulings_user_id_fkey` TO `schedulings_user_id_idx`;
+
+-- RenameIndex
+ALTER TABLE `sessions` RENAME INDEX `sessions_user_id_fkey` TO `sessions_user_id_idx`;
+
+-- RenameIndex
+ALTER TABLE `user_time_intervals` RENAME INDEX `user_time_intervals_user_id_fkey` TO `user_time_intervals_user_id_idx`;

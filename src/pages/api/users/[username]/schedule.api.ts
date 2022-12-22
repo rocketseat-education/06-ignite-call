44b9e339f8498160d5ae74cb5e30884811ec29c0
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-12-22 13:57:11 -0300

    Agendamento - Rota de agendamento

---
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -0,0 +1,69 @@
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+import { prisma } from '../../../../lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exist.' })
+  }
+
+  const createSchedulingBody = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    observations: z.string(),
+    date: z.string().datetime(),
+  })
+
+  const { name, email, observations, date } = createSchedulingBody.parse(
+    req.body,
+  )
+
+  const schedulingDate = dayjs(date).startOf('hour')
+
+  if (schedulingDate.isBefore(new Date())) {
+    return res.status(400).json({
+      message: 'Date is in the past.',
+    })
+  }
+
+  const conflictingScheduling = await prisma.scheduling.findFirst({
+    where: {
+      user_id: user.id,
+      date: schedulingDate.toDate(),
+    },
+  })
+
+  if (conflictingScheduling) {
+    return res.status(400).json({
+      message: 'There is another scheduling at at the same time.',
+    })
+  }
+
+  await prisma.scheduling.create({
+    data: {
+      name,
+      email,
+      observations,
+      date: schedulingDate.toDate(),
+      user_id: user.id,
+    },
+  })
+
+  return res.status(201).end()
+}
--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,9 +1,11 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
 import dayjs from 'dayjs'
+import { useRouter } from 'next/router'
 import { CalendarBlank, Clock } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { api } from '../../../../../lib/axios'
 import { ConfirmForm, FormActions, FormError, FormHeader } from './styles'
 
 const confirmFormSchema = z.object({
@@ -31,8 +33,20 @@ export function ConfirmStep({
     resolver: zodResolver(confirmFormSchema),
   })
 
-  function handleConfirmScheduling(data: ConfirmFormData) {
-    console.log(data)
+  const router = useRouter()
+  const username = String(router.query.username)
+
+  async function handleConfirmScheduling(data: ConfirmFormData) {
+    const { name, email, observations } = data
+
+    await api.post(`/users/${username}/schedule`, {
+      name,
+      email,
+      observations,
+      date: schedulingDate,
+    })
+
+    onCancelConfirmation()
   }
 
   const describedDate = dayjs(schedulingDate).format('DD[ de ]MMMM[ de ]YYYY')
